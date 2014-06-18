7ce4dc03fc1e38754bcf49f6f98684a2157c4c17
Daniel M <danielmauer@Daniels-MacBook-Pro.local>
2014-06-18 15:28:42 +0200

    geofencing.js edit

--- www/Geofencing.js
+++ www/Geofencing.js
@@ -1,4 +1,4 @@
-cordova.define("com.siteforum.plugins.geofencing.Geofencing", function(require, exports, module) { // cordova.define("com.siteforum.plugins.geofencing.Geofencing", function(require, exports, module) {var exec = require('cordova/exec');
+//cordova.define("com.siteforum.plugins.geofencing.Geofencing", function(require, exports, module) { // cordova.define("com.siteforum.plugins.geofencing.Geofencing", function(require, exports, module) {var exec = require('cordova/exec');
 
 var exec = require('cordova/exec');
 
